81a29ae208e29cd406832d24b75b8ebf494f5d21
dissipator <dissipator520@gmail.com>
2020-07-16 15:41:40 +0800

    调整config多余配置

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const config = require('./config').config();
 const TeleBot = require('telebot');
-const bot = new TeleBot(config.botToken);
 const { spawn } = require('child_process');
-const adminUsers = config.adminUsers;
-
 
 const { db } = require('./db');
 const { validate_fid, gen_count_body, count } = require('./src/gd');
 const { send_count, send_help, send_choice, send_task_info, sm, extract_fid, extract_from_text, reply_cb_query, tg_copy, send_all_tasks, send_bm_help, get_target_by_alias, gen_bookmark_choices, send_all_bookmarks, set_bookmark, unset_bookmark, clear_tasks, send_task_help, rm_task } = require('./src/tg');
 const { AUTH, ROUTER_PASSKEY, TG_IPLIST } = require('./config')
 const { tg_whitelist } = AUTH
+const { tg_token } = AUTH
+const { adminUsers } = AUTH
+const bot = new TeleBot(tg_token);
+
 
 const COPYING_FIDS = {}
 const counting = {}
@@ -41,115 +41,115 @@ bot.on('text', (msg) => {
     let is_shell = false
 
 
-	  const text = msg && msg.text && msg.text.trim()
-	  let username = msg && msg.from && msg.from.username
-	  msgs = username && String(username).toLowerCase()
-	  let user_id = msgs && msgs.from && msgs.from.id
-	  user_id = user_id && String(user_id).toLowerCase()
-	  if (!chat_id || !text || !tg_whitelist.some(v => {
-	    v = String(v).toLowerCase()
-	    return v === username || v === user_id
-	  })) return console.warn('异常请求')
+      const text = msg && msg.text && msg.text.trim()
+      let username = msg && msg.from && msg.from.username
+      msgs = username && String(username).toLowerCase()
+      let user_id = msgs && msgs.from && msgs.from.id
+      user_id = user_id && String(user_id).toLowerCase()
+      if (!chat_id || !text || !tg_whitelist.some(v => {
+        v = String(v).toLowerCase()
+        return v === username || v === user_id
+      })) return console.warn('异常请求')
 
-	    const fid = extract_fid(text) || extract_from_text(text)
-	    const no_fid_commands = ['/task', '/help', '/bm']
-	    if (!no_fid_commands.some(cmd => text.startsWith(cmd)) && !validate_fid(fid)) {
-	      // console.log("is_shell:"+is_shell);
-	      sm({ chat_id, text: '未识别出分享ID' })
-	      is_shell = true
-	    }
-	    if (text.startsWith('/help')) return send_help(chat_id)
-	    if (text.startsWith('/bm')) {
-	      const [cmd, action, alias, target] = text.split(' ').map(v => v.trim())
-	      if (!action) return send_all_bookmarks(chat_id)
-	      if (action === 'set') {
-	        if (!alias || !target) return sm({ chat_id, text: '别名和目标ID不能为空' })
-	        if (alias.length > 24) return sm({ chat_id, text: '别名不要超过24个英文字符长度' })
-	        if (!validate_fid(target)) return sm({ chat_id, text: '目标ID格式有误' })
-	        set_bookmark({ chat_id, alias, target })
-	      } else if (action === 'unset') {
-	        if (!alias) return sm({ chat_id, text: '别名不能为空' })
-	        unset_bookmark({ chat_id, alias })
-	      } else {
-	        send_bm_help(chat_id)
-	      }
-	    } else if (text.startsWith('/count')) {
-	      if (counting[fid]) return sm({ chat_id, text: fid + ' 正在统计，请稍等片刻' })
-	      try {
-	        counting[fid] = true
-	        const update = text.endsWith(' -u')
-	        send_count({ fid, chat_id, update })
-	      } catch (err) {
-	        console.error(err)
-	        sm({ chat_id, text: fid + ' 统计失败：' + err.message })
-	      } finally {
-	        delete counting[fid]
-	      }
-	    } else if (text.startsWith('/copy')) {
-	      let target = text.replace('/copy', '').replace(' -u', '').trim().split(' ').map(v => v.trim())[1]
-	      target = get_target_by_alias(target) || target
-	      if (target && !validate_fid(target)) return sm({ chat_id, text: `目标ID ${target} 格式不正确` })
-	      const update = text.endsWith(' -u')
-	      tg_copy({ fid, target, chat_id, update }).then(task_id => {
-	        task_id && sm({ chat_id, text: `开始复制，任务ID: ${task_id} 可输入 /task ${task_id} 查询进度` })
-	      })
-	    } else if (text.startsWith('/task')) {
-	      let task_id = text.replace('/task', '').trim()
-	      if (task_id === 'all') {
-	        return send_all_tasks(chat_id)
-	      } else if (task_id === 'clear') {
-	        return clear_tasks(chat_id)
-	      } else if (task_id === '-h') {
-	        return send_task_help(chat_id)
-	      } else if (task_id.startsWith('rm')) {
-	        task_id = task_id.replace('rm', '')
-	        task_id = parseInt(task_id)
-	        if (!task_id) return send_task_help(chat_id)
-	        return rm_task({ task_id, chat_id })
-	      }
-	      task_id = parseInt(task_id)
-	      if (!task_id) {
-	        const running_tasks = db.prepare('select id from task where status=?').all('copying')
-	        if (!running_tasks.length) return sm({ chat_id, text: '当前暂无运行中的任务' })
-	        return running_tasks.forEach(v => send_task_info({ chat_id, task_id: v.id }).catch(console.error))
-	      }
-	      send_task_info({ task_id, chat_id }).catch(console.error)
-	    } else if (text.includes('drive.google.com/') || validate_fid(text)) {
-	    	return send_choice({ fid: fid || text, chat_id }).catch(console.error)
-	    	// let replyMarkup = bot.inlineKeyboard([
-		    //     [
-		    //         bot.inlineButton('文件统计', {callback: `count ${fid}` }),
-		    //         bot.inlineButton('开始复制', {callback: `copy ${fid}` })
-		    //     ].concat(gen_bookmark_choices(fid))
-	    	// ]);
-	    	// return bot.sendMessage(id, `识别出分享ID ${fid}，请选择动作`, {replyMarkup});
-	    } else {
-	    	is_shell = true
-	    	// sm({ chat_id, text: '暫不支持此命令' })
-	  	}
+        const fid = extract_fid(text) || extract_from_text(text)
+        const no_fid_commands = ['/task', '/help', '/bm']
+        if (!no_fid_commands.some(cmd => text.startsWith(cmd)) && !validate_fid(fid)) {
+          // console.log("is_shell:"+is_shell);
+          sm({ chat_id, text: '未识别出分享ID' })
+          is_shell = true
+        }
+        if (text.startsWith('/help')) return send_help(chat_id)
+        if (text.startsWith('/bm')) {
+          const [cmd, action, alias, target] = text.split(' ').map(v => v.trim())
+          if (!action) return send_all_bookmarks(chat_id)
+          if (action === 'set') {
+            if (!alias || !target) return sm({ chat_id, text: '别名和目标ID不能为空' })
+            if (alias.length > 24) return sm({ chat_id, text: '别名不要超过24个英文字符长度' })
+            if (!validate_fid(target)) return sm({ chat_id, text: '目标ID格式有误' })
+            set_bookmark({ chat_id, alias, target })
+          } else if (action === 'unset') {
+            if (!alias) return sm({ chat_id, text: '别名不能为空' })
+            unset_bookmark({ chat_id, alias })
+          } else {
+            send_bm_help(chat_id)
+          }
+        } else if (text.startsWith('/count')) {
+          if (counting[fid]) return sm({ chat_id, text: fid + ' 正在统计，请稍等片刻' })
+          try {
+            counting[fid] = true
+            const update = text.endsWith(' -u')
+            send_count({ fid, chat_id, update })
+          } catch (err) {
+            console.error(err)
+            sm({ chat_id, text: fid + ' 统计失败：' + err.message })
+          } finally {
+            delete counting[fid]
+          }
+        } else if (text.startsWith('/copy')) {
+          let target = text.replace('/copy', '').replace(' -u', '').trim().split(' ').map(v => v.trim())[1]
+          target = get_target_by_alias(target) || target
+          if (target && !validate_fid(target)) return sm({ chat_id, text: `目标ID ${target} 格式不正确` })
+          const update = text.endsWith(' -u')
+          tg_copy({ fid, target, chat_id, update }).then(task_id => {
+            task_id && sm({ chat_id, text: `开始复制，任务ID: ${task_id} 可输入 /task ${task_id} 查询进度` })
+          })
+        } else if (text.startsWith('/task')) {
+          let task_id = text.replace('/task', '').trim()
+          if (task_id === 'all') {
+            return send_all_tasks(chat_id)
+          } else if (task_id === 'clear') {
+            return clear_tasks(chat_id)
+          } else if (task_id === '-h') {
+            return send_task_help(chat_id)
+          } else if (task_id.startsWith('rm')) {
+            task_id = task_id.replace('rm', '')
+            task_id = parseInt(task_id)
+            if (!task_id) return send_task_help(chat_id)
+            return rm_task({ task_id, chat_id })
+          }
+          task_id = parseInt(task_id)
+          if (!task_id) {
+            const running_tasks = db.prepare('select id from task where status=?').all('copying')
+            if (!running_tasks.length) return sm({ chat_id, text: '当前暂无运行中的任务' })
+            return running_tasks.forEach(v => send_task_info({ chat_id, task_id: v.id }).catch(console.error))
+          }
+          send_task_info({ task_id, chat_id }).catch(console.error)
+        } else if (text.includes('drive.google.com/') || validate_fid(text)) {
+            return send_choice({ fid: fid || text, chat_id }).catch(console.error)
+            // let replyMarkup = bot.inlineKeyboard([
+            //     [
+            //         bot.inlineButton('文件统计', {callback: `count ${fid}` }),
+            //         bot.inlineButton('开始复制', {callback: `copy ${fid}` })
+            //     ].concat(gen_bookmark_choices(fid))
+            // ]);
+            // return bot.sendMessage(id, `识别出分享ID ${fid}，请选择动作`, {replyMarkup});
+        } else {
+            is_shell = true
+            // sm({ chat_id, text: '暫不支持此命令' })
+        }
 
     if (is_shell) {
         console.log('run shell')
         msg.reply.text('$:   '+msg.text);
         const shell = spawn(words[0],args).on('error', function( err ){
-        	msg.reply.text('error while executing:'+words[0]);
-        	msg.reply.text(err);
-    	});
+            msg.reply.text('error while executing:'+words[0]);
+            msg.reply.text(err);
+        });
 
         if(shell){
 
-    	   shell.stdout.on('data', (data) => {
-          	msg.reply.text(`stdout:\n ${data}`);
-    	   });
+           shell.stdout.on('data', (data) => {
+            msg.reply.text(`stdout:\n ${data}`);
+           });
 
-    	   shell.stderr.on('data', (data) => {
-        	msg.reply.text(`stderr: ${data}`);
-    	   });
+           shell.stderr.on('data', (data) => {
+            msg.reply.text(`stderr: ${data}`);
+           });
 
-    	   shell.on('close', (code) => {
-        	msg.reply.text(`shell exited with code ${code}`);
-    	   });
-	}
+           shell.on('close', (code) => {
+            msg.reply.text(`shell exited with code ${code}`);
+           });
+    }
     }
 });
 
@@ -190,9 +190,4 @@ bot.on('callbackQuery', msg => {
     return bot.answerCallbackQuery(msg.id, `Inline button callback: ${ msg.data }`, true);
 });
 
-
-
 bot.start();
-
-
-

0c3a8473c365472bd90531d63e7118f7ec162667
dissipator <dissipator@users.noreply.github.com>
2020-07-28 07:54:06 +0800

    Update start.sh
    4

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ adduser  gd -u 20001 -D -S -s /bin/bash -G root
 echo -e "${USERPWD}\n${USERPWD}" | passwd root
 echo -e "${USERPWD}\n${USERPWD}" | passwd gd
 chmod 4755 /bin/busybox
-if [ -z $BOT_TOKEN ]
+if [ -z $BOT_TOKEN ];then
 echo no args
 else
 sed -i "s/bot_token/${BOT_TOKEN}/g" /gd-utils/config.js

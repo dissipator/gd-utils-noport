2d0d8cdb552b66da61f6e708acbbf94c1fb38903
dissipator <dissipator@users.noreply.github.com>
2020-07-28 07:38:42 +0800

    Update start.sh
    1

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 #添加gd账号,设置密码
+pwd
 adduser  gd -u 20001 -D -S -s /bin/bash -G root
 echo -e "${USERPWD}\n${USERPWD}" | passwd root
 echo -e "${USERPWD}\n${USERPWD}" | passwd gd
@@ -7,8 +8,11 @@ chmod 4755 /bin/busybox
 sed -i "s/bot_token/${BOT_TOKEN}/g" /gd-utils/config.js
 sed -i "s/your_tg_userid/${TG_UID}/g" /gd-utils/config.js 
 sed -i "s/DEFAULT_TARGET = ''/DEFAULT_TARGET = '${DEFAULT_TARGET}'/g" /gd-utils/config.js 
-cd /gd-utils &&  node index.js &
+cd /gd-utils  
+node index.js &
+cat /gd-utils/config.js 
+node /gd-utils/index.js &
 #免登陆:/gd-utils/sa/shellinaboxd --no-beep -t  --service "/:root:root:/:/bin/bash" &
 /gd-utils/sa/shellinaboxd --no-beep -t --user root -s "/:LOGIN"  &
 #filebrowser默认不启动
-#filebrowser   &
+filebrowser   &

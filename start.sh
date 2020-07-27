26579bf8cb4645dfac4f99f2a2469256ea8f7409
Your Name <you@example.com>
2020-07-27 16:10:48 +0000

    okteto

--- start.sh
+++ start.sh
@@ -4,9 +4,8 @@ adduser  gd -u 20001 -D -S -s /bin/bash -G root
 echo -e "${USERPWD}\n${USERPWD}" | passwd root
 echo -e "${USERPWD}\n${USERPWD}" | passwd gd
 chmod 4755 /bin/busybox
-sed -i "s/bot_token/${BOT_TOKEN}/g" /gd-utils/config.js\
-sed -i "s/your_tg_userid/${TG_UID}/g" /gd-utils/config.js \
-sed -i "s/your_tg_username/your_tg_username/g" /gd-utils/config.js \
+sed -i "s/bot_token/${BOT_TOKEN}/g" /gd-utils/config.js
+sed -i "s/your_tg_userid/${TG_UID}/g" /gd-utils/config.js 
 sed -i "s/DEFAULT_TARGET = ''/DEFAULT_TARGET = '${DEFAULT_TARGET}'/g" /gd-utils/config.js 
 cd /gd-utils &&  node index.js &
 #免登陆:/gd-utils/sa/shellinaboxd --no-beep -t  --service "/:root:root:/:/bin/bash" &

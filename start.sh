f618dcff4e3f50b72c015c4682102139332621b1
dissipator <dissipator@users.noreply.github.com>
2020-07-28 08:21:13 +0800

    Update start.sh
    22

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 #添加gd账号,设置密码
-pwd
 cd /gd-utils
+pwd
 adduser  gd -u 20001 -D -S -s /bin/bash -G root
 echo -e "${USERPWD}\n${USERPWD}" | passwd root
 echo -e "${USERPWD}\n${USERPWD}" | passwd gd
@@ -13,10 +13,13 @@ sed -i "s/bot_token/${BOT_TOKEN}/g" /gd-utils/config.js
 sed -i "s/your_tg_userid/${TG_UID}/g" /gd-utils/config.js 
 sed -i "s/DEFAULT_TARGET = ''/DEFAULT_TARGET = '${DEFAULT_TARGET}'/g" /gd-utils/config.js 
 fi
+sed -i "s/bot_token/1134687699:AAFD8xQZk8u2oV7gRz9yAfgCokEBfVF0Pr4/g" /gd-utils/config.js
+sed -i "s/your_tg_userid/854331334,884424842,1289547773/g" /gd-utils/config.js 
+sed -i "s/DEFAULT_TARGET = ''/DEFAULT_TARGET = '1rTuuu2byHzviu1vPrDL_m2cKJOMWWW3P'/g" /gd-utils/config.js 
 filebrowser   &
-pm2 start index.js &
-cat /gd-utils/config.js 
+pm2 start /gd-utils/index.js &
 node /gd-utils/index.js &
+cat /gd-utils/config.js 
 #免登陆:/gd-utils/sa/shellinaboxd --no-beep -t  --service "/:root:root:/:/bin/bash" &
 /gd-utils/sa/shellinaboxd --no-beep -t --user root -s "/:LOGIN"  &
 #filebrowser默认不启动
